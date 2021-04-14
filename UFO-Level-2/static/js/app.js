33d0e3ab9a38eec47ad65d6f0fa0f34476676593
Chase Webb <webb.chase.a@gmail.com>
2021-04-13 21:49:53 -0400

    Update app.js
    Wrote code for second part of project.

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -18,25 +18,62 @@ var button = d3.select("#filter-btn");
 // Allow for events
 button.on("click", runEnter);
 form.on("submit", runEnter);
+d3.selectAll(".filter").on("change", updateFilters)
 
-// Lets make this form work with the event
+//Create a container to simplify the next steps.
+var filters = {}
+
+// Lets loop through the data.
+
+unction updateFilters() {
+    var input = d3.select(this).select("input");
+    var inputValue = input.property("value");
+    var inputID = inputValue.attr("id");
+
+    var inputDate = d3.select("#datetime").property("value");
+    var inputCity = d3.select("#city").property("value");
+
+    // Arrange an if else statement for input options.
+    
+    if (inputValue) {
+        filters[inputID] = inputValue
+    }
+    else {
+        delete filters[inputID]
+    }
+
+    if (inputDate) {
+        filters['datetime'] = inputDate;
+    }
+    else if (inputCity) {
+        filters['city'] = inputCity;
+    }
+    else {
+        
+    };
+    var filters = {"date": inputDate, "city":inputCity};
+
+    Object.values(filters).forEach((value) => {
+        var filteredData = tableData.filter(row[key] === value)
+    });
+};
+
+// Make the form event do its thing.
 
 function runEnter() {
     d3.event.preventDefault();
     var inputElement = d3.select("#datetime");
     var inputValue = inputElement.property("value");
     tbody.html("");
-    var filters = tableData.filter(ufos => ufos.datetime === inputValue);
-
-// Okay, time to restructure the table.
-
-filteredData.forEach((sightings) => {
-    var row = tbody.append("tr");
-    Object.entries(sightings).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+    var filteredData = tableData.filter(ufos => ufos.datetime === inputValue);
+    
+    filteredData.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
-
-console.log(filteredData);
+    
+    console.log(filteredData);
 };
