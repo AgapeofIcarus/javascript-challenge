94c3c4e1ce5040fdcc4a49115f857cb06eb4e9a4
Chase Webb <webb.chase.a@gmail.com>
2021-04-13 20:09:14 -0400

    Update app.js
    Made progress on app

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,28 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
+// Create loop for data in reports.
 var tbody = d3.select("tbody");
+tableData.forEach((sightings) => {
+    var row = tbody.append("tr");
+    Object.entries(sightings).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+// Select the form and button
+var form = d3.select(".form-control");
+var button = d3.select("#click-me");
 
 console.log(data);
 
+data.forEach(function(sightings) {
+  console.log(sightings);
+  var row = tbody.append("tr");
+  Object.entries(weatherReport).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
